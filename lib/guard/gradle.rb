b47ce72847a49c8915a3d72574d68d8391d8d78f
Andrew Glover <aglover@netflix.com>
2014-05-28 12:15:33 -0700

    added notifications

--- lib/guard/gradle.rb
+++ lib/guard/gradle.rb
@@ -4,20 +4,38 @@ require 'guard/plugin'
 
 module Guard
 	class Gradle < Plugin
+		
+		DEF_CMD = './gradlew test'
+
 		def run_on_changes(paths)
 			if(paths.size == 1) && (Dir.glob("src/test/**/#{paths[0]}*").size > 0)
-				fire_command("./gradlew test -Dtest.single=#{paths[0]}")
+				fire_command("#{DEF_CMD} -Dtest.single=#{paths[0]}")
       		else
       			run_all
       		end
 		end
 
 		def run_all
-			fire_command('./gradlew test')
+			fire_command DEF_CMD
 		end
 
 		def fire_command(command)
-			spawn(command)
+			result = system command
+    		summary = result ? 'Success' : 'Failure'
+    		image = result ? :success : :failed
+    		notify(summary, image)
 		end
+
+		 def notify(summary, image)
+    		::Guard::Notifier.notify(summary, title: 'Nebula Test Results', image: image)
+    	end
+
+	    def start
+	    	fire_command DEF_CMD
+	    end
+
+	    def run_all
+	    	fire_command DEF_CMD
+	    end
 	end
 end

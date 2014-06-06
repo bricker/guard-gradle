ef79b0423962d677e968c80f4f28505a6348eb0c
Andrew Glover <aglover@netflix.com>
2014-06-05 19:13:11 -0700

    added multi-project support, 1st pass. Fixes #3

--- lib/guard/gradle.rb
+++ lib/guard/gradle.rb
@@ -7,8 +7,25 @@ module Guard
 		
     DEF_CMD = './gradlew test'
 
+    @multi_projs = false
+
+    def initialize(options = {})
+      if options.has_key? :multi_projects
+        @multi_projs = options[:multi_projects]
+      end
+      super
+    end
+
     def run_on_changes(paths)
-      if(paths.size == 1) && (Dir.glob("src/test/**/#{paths[0]}*").size > 0)
+      if paths.size == 1 && @multi_projs
+        project = paths[0].split('/')[0]
+        file = paths[0].split('/')[1]    
+        if Dir.glob("#{project}/src/test/**/#{file}*").size > 0
+          fire_command("#{DEF_CMD} -p #{project} -Dtest.single=#{file} --daemon")
+        else
+          run_all
+        end
+      elsif(paths.size == 1) && (Dir.glob("src/test/**/#{paths[0]}*").size > 0)
         fire_command("#{DEF_CMD} -Dtest.single=#{paths[0]} --daemon")
       else
         run_all
@@ -20,6 +37,7 @@ module Guard
     end
 
     def fire_command(command)
+      UI.info "running #{command}"
       result = system command
       summary = result ? 'Success' : 'Failure'
       image = result ? :success : :failed

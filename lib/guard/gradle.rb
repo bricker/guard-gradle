615df05ec44908426b6f586f3b01cfa9a6e920de
Dave Hendler <sollaires@gmail.com>
2014-07-29 15:32:24 -0700

    Fixing instance variable definitions

--- lib/guard/gradle.rb
+++ lib/guard/gradle.rb
@@ -4,14 +4,13 @@ require 'guard/plugin'
 
 module Guard
   class Gradle < Plugin
-		
-    @command = './gradlew'
-    @task = 'test'
-
-    @multi_projs = false
-    @flags = false
 
     def initialize(options = {})
+      @command = './gradlew'
+      @task = 'test'
+      @multi_projs = false
+      @flags = false
+
       if options.has_key? :multi_project
         @multi_projs = options[:multi_project]
       end
@@ -38,19 +37,19 @@ module Guard
         project = paths[0].split('/')[0]
         file = paths[0].split('/')[1]    
         if Dir.glob("#{project}/src/test/**/#{file}*").size > 0
-          fire_command("#{DEF_CMD} -p #{project} -Dtest.single=#{file} --daemon")
+          fire_command("#{@DEF_CMD} -p #{project} -Dtest.single=#{file} --daemon")
         else
           run_all
         end
       elsif(paths.size == 1) && (Dir.glob("src/test/**/#{paths[0]}*").size > 0)
-        fire_command("#{DEF_CMD} -Dtest.single=#{paths[0]} --daemon")
+        fire_command("#{@DEF_CMD} -Dtest.single=#{paths[0]} --daemon")
       else
         run_all
       end
     end
 
     def run_all
-      fire_command DEF_CMD
+      fire_command @DEF_CMD
     end
 
     def fire_command(command)
@@ -66,11 +65,11 @@ module Guard
     end
 
     def start
-      fire_command DEF_CMD
+      fire_command @DEF_CMD
     end
 
     def run_all
-      fire_command DEF_CMD
+      fire_command @DEF_CMD
     end
   end
 end
